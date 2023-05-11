14b9106eb6e5cc3e870847007a984c9e8ed935d2
Erasmo Prosciutto <erasmo.prosciutto@gmail.com>
2023-05-11 16:16:17 +0200

    Base di partenza per una schermata di login: -benvenuto -email -password -bottone accedi
    Base di partenza per una logica di verifica di email e password

--- app/src/main/java/com/example/robotinteraction/MainActivity.java
+++ app/src/main/java/com/example/robotinteraction/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.robotinteraction;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set degli ID per i componenti grafici
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        // Gestire evento sul pulsante accedi"
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                // Verificare i dati con il database
+                if(!verifyCredentials(email,password)){
+                    //messaggio di errore, inserire nuovamente i dati
+                }
+                else{
+                    //procedere...
+                }
+            }
+        });
     }
+
+    private boolean verifyEmail(String email){
+        //connessione al database e verifica della mail
+        return false;
+    }
+
+    private boolean verifyPassword(String password){
+        //connessione al database e verifica della password
+        return false;
+    };
+    private boolean verifyCredentials(String email, String password){
+        if(!verifyEmail(email) || !verifyPassword(password)){
+            return false;
+        }
+        return true;
+    };
 }
+
+//
