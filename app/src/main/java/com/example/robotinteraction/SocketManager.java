98dc1738bf28c4d5b89820b47a3fd92bd952bf4e
Erasmo Prosciutto <erasmo.prosciutto@gmail.com>
2023-05-18 13:17:56 +0200

    Fixing dei Constraints sulla grafica xml delle activity: main,welcome,interview,signup

--- app/src/main/java/com/example/robotinteraction/SocketManager.java
+++ app/src/main/java/com/example/robotinteraction/SocketManager.java
@@ -21,8 +21,8 @@ public class SocketManager {
 
     private MessageListener messageListener;
 
-    private static final String SERVER_IP = "00.00.00";
-    private static final int SERVER_PORT = 0000;
+    private static final String SERVER_IP = "127.0.0.1";
+    private static final int SERVER_PORT = 8080;
 
     private SocketManager() {
         // Nessun costruttore esterno necessario
